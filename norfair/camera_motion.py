c8bfbf40aaf01fec694024f0f1134121a1c17555
tomo-makes <tmsojiner@gmail.com>
2023-01-08 21:20:19 +0900

    modifined camera_motion.py

--- norfair/camera_motion.py
+++ norfair/camera_motion.py
@@ -271,6 +271,38 @@ def _get_sparse_flow(
     curr_pts = curr_pts[idx].reshape((-1, 2))
     return curr_pts, prev_pts
 
+# Motion estimation
+# for other than GFTT
+def _get_general_sparse_flow(
+    gray_next,
+    gray_prvs,
+    prev_pts=None,
+    max_points=300,
+    min_distance=15,
+    block_size=3,
+    mask=None,
+    quality_level=0.01,
+):
+    if prev_pts is None:
+        # get points
+        prev_pts = cv2.goodFeaturesToTrack(
+            gray_prvs,
+            maxCorners=max_points,
+            qualityLevel=quality_level,
+            minDistance=min_distance,
+            blockSize=block_size,
+            mask=mask,
+        )
+
+    # compute optical flow
+    curr_pts, status, err = cv2.calcOpticalFlowPyrLK(
+        gray_prvs, gray_next, prev_pts, None
+    )
+    # filter valid points
+    idx = np.where(status == 1)[0]
+    prev_pts = prev_pts[idx].reshape((-1, 2))
+    curr_pts = curr_pts[idx].reshape((-1, 2))
+    return curr_pts, prev_pts
 
 class MotionEstimator:
     """
